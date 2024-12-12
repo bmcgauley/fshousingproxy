e05515449c3ad08ab7ded2fbc12ecc5505ff6869
bmcgauley <brian@mcgauley.com>
2024-12-12 14:44:38 -0800

    add GET

--- api/update-feed.js
+++ api/update-feed.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS" || req.method === "GET") {
