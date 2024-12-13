46acceb9b3a30976f11ebce60513f0a739bf1d13
bmcgauley <brian@mcgauley.com>
2024-12-13 14:04:14 -0800

    dunno

--- x.js
+++ x.js
@@ -9,10 +9,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-app.get("/fscollegian", (req, res) => {
+app.get("/api/fscollegian", (req, res) => {
   request({ url: "https://fscollegian.com/feed/" }, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      return res.status(500).json({ type: "error", message: err.message });
+      return res.status(500).json({ type: "error", message: error.message });
     }
     res.set("Content-Type", "application/rss+xml");
     res.send(Buffer.from(body));
