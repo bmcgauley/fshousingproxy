726547dac852e107f430c3e93b67d3c4e1bfd008
bmcgauley <brian@mcgauley.com>
2024-12-18 14:49:49 -0800

    Update CORS configuration to allow requests from the new frontend origin 'https://fresnostatehousing.org'

--- utils/cors-helper.js
+++ utils/cors-helper.js
@@ -1,6 +1,6 @@
 // cors-helper.js
 export default function setCorsHeaders(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://fshousingproxy.vercel.app/'); // Replace with your frontend's origin
+    res.setHeader('Access-Control-Allow-Origin', 'https://fresnostatehousing.org'); // Replace with your frontend's origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
